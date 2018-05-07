7eea96686c846b426c5b66cfee10ebd1118ee58e
Gustavo Castilhos Garcia <guscastles@gmail.com>
2018-05-07 20:49:24 +1000

    Minor refactoring

--- test/test_game_time.py
+++ test/test_game_time.py
@@ -8,4 +8,7 @@ def test_overall_time_offs():
     stats = gt.data_stats(*gt.valid_data(data))
     assert len(stats) == 4 and len(stats.columns) == 10
     assert 'turns as goalie' in stats.index
-    assert dict(stats.loc[:, 'Nicholas']) == {'matches played': 5.0, 'average time played': 33.4, 'total time played': 167.0, 'turns as goalie': 1.0}
+    assert dict(stats.loc[:, 'Nicholas']) == {'matches played': 5.0,
+                                              'average time played': 33.4,
+                                              'total time played': 167.0,
+                                              'turns as goalie': 1.0}
